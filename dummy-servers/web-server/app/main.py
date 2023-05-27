e738ea44370764157c454122b2c9b04d2fe98158
Natu17 <nlilienthal@itba.edu.ar>
2023-05-27 16:45:59 -0300

    connect web-server with database

--- dummy-servers/web-server/app/main.py
+++ dummy-servers/web-server/app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
-
+from db import database, Items
 app = FastAPI(title="FastAPI, Docker")
 
 
 @app.get("/")
 def read_root():
     return {"msg": "Hello World!"}
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await Items.objects.get_or_create(name="test", price=10.0)
+
+@app.get("/items")
+async def read_all_items():
+    return await Items.objects.all()
