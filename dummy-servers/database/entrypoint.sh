fb409d35dc87ff0f289ac170cb7819e801dbe187
oserpe <oserpe@itba.edu.ar>
2023-05-28 15:43:40 +0000

    :sparkles: add database logging without agent

--- dummy-servers/database/entrypoint.sh
+++ dummy-servers/database/entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /app/filebeat-7.17.10-linux-x86_64 && ./filebeat -e &
 
-cd / && ./docker-entrypoint.sh postgres -c log_statement=all -c log_directory=/var/lib/postgresql/data/log -c logging_collector=true
+cd / && ./docker-entrypoint.sh postgres -c log_statement=all -c log_directory=/app/logs -c logging_collector=true
